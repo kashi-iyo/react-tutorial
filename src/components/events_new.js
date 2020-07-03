6b3f10f19186af168b5cb602070d582f5a55ebaa
kasi-iyo <snakeman143@gmail.com>
2020-07-03 10:51:00 +0900

    新規作成画面、編集画面のスタイリング

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
 import { postEvent } from '../actions' //readEvents関数のインポート
 
 class EventsNew extends Component {
@@ -16,10 +19,18 @@ class EventsNew extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
+      // <div>
+      //   <input {...input} placeholder={label} type={type} />
+      //   {touched && error && <span>{error}</span>}
+      // </div>
     )
   }
 
@@ -31,16 +42,15 @@ class EventsNew extends Component {
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
 
+    const style = { margin: 12 }
+
     return (
       <div>
         <form onSubmit={handleSubmit(this.onSubmit)}>
           <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
           <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-
-          <div>
-            <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-            <Link to="/">Cancel</Link>
-          </div>
+          <RaisedButton label="作成" type="submit" style={style} disabled={pristine || submitting || invalid} />
+          <RaisedButton label="戻る" style={style} containerElement={<Link to="/" />} />
         </form>
       </div>
     )

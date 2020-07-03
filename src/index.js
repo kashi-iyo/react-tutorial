3502a51c41cbfc5dc9302d2154a9e3ac677be605
kasi-iyo <snakeman143@gmail.com>
2020-07-03 10:33:08 +0900

    material-uiパッケージの追加。一覧ページをスタイリング

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux' // Provierは作成したstoreを全コ�
 import thunk from 'redux-thunk' //redux-thunkはミドルウェアに該当する
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css'
 import reducer from './reducers'  // 状態を変更する機能が記述してあるファイル
@@ -23,15 +24,17 @@ const store = createStore(reducer, enhancer)
 
 // Providerコンポーネント内に、他のコンポーネントをラップしてあげるだけでOK。
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
